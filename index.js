32c678ab4e5265facd3743ccdf25ccda6aaf24d5
alanx.chiang <alanx.chiang@intel.corp-partner.google.com>
2018-03-16 15:25:01 +0800

    Test for 403 forbidden issue

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const url = 'https://tw.shop.com/'
 var options = {
   url: "https://tw.shop.com/%E4%BB%8A%E5%91%A8%E5%A5%97%E6%9B%B8+%E8%A1%8C%E9%8A%B7%E9%9D%A0%E7%A4%BE%E7%BE%A4+%E8%A1%8C%E9%8A%B7%E6%8F%9B%E8%85%A6%E8%A2%8B-1436701386-p+.xhtml?tkr=180117021137",
   headers: {
+    accept : '*/*'
     'User-Agent' : 'Chrome/35.0.1916.153' // optional headers
   }
 };
